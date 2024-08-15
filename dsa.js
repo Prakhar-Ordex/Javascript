75a4919ea39949b8e39fd75d3aedf8558564b86a
Prakhar Tripathi <prakhar.tripathi.ordex@gmail.com>
2024-08-15 16:08:31 +0530

    update topic

--- dsa.js
+++ dsa.js
@@ -21,14 +21,35 @@
 
 // map -> store data in key and value pairs 
 
-let myMap = new Map([["a1","prakhar"],["a2","shlok"],["a3","shlok"]]);
-myMap.set("a2", "prakhar")
-myMap.delete("a2");
-console.log(myMap);
-console.log(myMap.get("a1"));
+// let myMap = new Map([["a1","prakhar"],["a2","shlok"],["a3","shlok"]]);
+// myMap.set("a2", "prakhar")
+// myMap.delete("a2");
+// console.log(myMap);
+// console.log(myMap.get("a1"));
 // for (let [key, value] of myMap) {
 //     console.log(`key: ${key} value: ${value}`)
 // }
 
-myMap.forEach((value, key) => { console.log(`key: ${key} value: ${value}`) });
+// myMap.forEach((value, key) => { console.log(`key: ${key} value: ${value}`) });
 
+
+// weakSet => {...} store object only not itrable
+// only store object 
+// it cannot be itreate 
+// let ws = new WeakSet();
+// let obj = {name:"prakhar"};
+// let obj1 = {age:"20"};
+// ws.add(obj)
+// ws.add(obj1)
+// // ws.delete(obj)
+// console.log(ws)
+// console.log(ws.has(obj))
+
+// WeakMap => not itrable store only object
+
+// let wm = new WeakMap();
+// let obj = { name: "prakhar" };
+// let obj1 = { age: "20" };
+// wm.set(obj, obj1)
+// console.log(wm.has(obj))
+// console.log(wm)
